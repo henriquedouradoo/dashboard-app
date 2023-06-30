40724130827f0d27acf2dc4dc5d7fc148704430e
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-06-29 22:59:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const sideMenu = document.querySelector('aside');
-const menuBtn = document.querySelector('menu-btn');
-const closeBtn = document.querySelector('close-btn');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
 
-const darkMode = document.querySelector('dark-mode');
+const darkMode = document.querySelector('.dark-mode');
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
@@ -10,6 +10,12 @@ menuBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
 });
 
 Orders.forEach(order =>{
